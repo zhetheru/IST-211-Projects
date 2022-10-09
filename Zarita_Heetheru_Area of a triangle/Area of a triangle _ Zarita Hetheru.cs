e6e59edc0c0e3efaaadfc00792eb92893b0d8461
zhetheru <58991148+zhetheru@users.noreply.github.com>
2022-10-09 19:12:26 -0400

    Add files via upload

---
+++ Zarita_Heetheru_Area of a triangle/Area of a triangle _ Zarita Hetheru.cs
@@ -0,0 +1,20 @@
+﻿//
+//programmer name - Zarita Lee Hetheru
+//Project number - Area of Triangle Program - In class Program
+//Date Due - 9-14-2022
+//Date Submitted - 9-14-2022
+//
+namespace Zarita_Heetheru_Area_of_a_triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           double Base = 5;
+           double height = 6;
+           double area = .5 * Base * height;
+            Console.WriteLine(area);
+        }
+    }
+}
+// Rest of the Class
